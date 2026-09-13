8bc8a5578fec3af1184ed65ed68adce9b8d69a84
Christian Haitian <christianhaitian0@gmail.com>
2026-09-13 11:11:27 -0400

    Update nds.sh again to move sed for DSperate lower in the gate

--- DSperate/scripts/nds.sh
+++ DSperate/scripts/nds.sh
@@ -24,10 +24,6 @@ if [[ "$1" == "drastic" ]]; then
 
   sudo systemctl restart ogage &
 elif [[ "$1" == "dsperate" ]]; then
-  sed -i "/saves =/c\saves = /${directory}/nds" /${directory}/nds/dsperate/dsperate.ini
-  sed -i "/states =/c\states = /${directory}/nds" /${directory}/nds/dsperate/dsperate.ini
-  sed -i "/cheats =/c\cheats = /${directory}/nds" /${directory}/nds/dsperate/dsperate.ini
-
   # The DSperate standalone emulator does not support 7z archive files.  We'll take care of that here
   game="$2"
   ext="${2##*.}"
@@ -72,6 +68,10 @@ elif [[ "$1" == "dsperate" ]]; then
   fi
 
   ln -sfn /${directory}/nds/dsperate /home/ark/.config/
+
+  sed -i "/saves =/c\saves = /${directory}/nds" /${directory}/nds/dsperate/dsperate.ini
+  sed -i "/states =/c\states = /${directory}/nds" /${directory}/nds/dsperate/dsperate.ini
+  sed -i "/cheats =/c\cheats = /${directory}/nds" /${directory}/nds/dsperate/dsperate.ini
 
   /opt/DSperate/dsperate "$game" --bios9 /${directory}/bios/nds_bios9.bin --bios7 /${directory}/bios/nds_bios7.bin --firmware /${directory}/bios/nds_firmware.bin
 
